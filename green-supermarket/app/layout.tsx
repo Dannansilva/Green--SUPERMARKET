a3204b3039fd94463b1877b4fa6e34dc852f8be8
Dannansilva <dannangunathilaka@gmail.com>
2023-12-06 21:42:54 +0530

    navbar update

--- green-supermarket/app/layout.tsx
+++ green-supermarket/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/common ui/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
