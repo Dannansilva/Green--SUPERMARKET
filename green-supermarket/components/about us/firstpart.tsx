2dd0dcc4bf87b498f5ba3571df5c93cf242a25d7
Shavinka06 <91057321+Shavinka06@users.noreply.github.com>
2023-12-12 17:14:17 +0530

    Completed the About us page

--- green-supermarket/components/about us/firstpart.tsx
+++ green-supermarket/components/about us/firstpart.tsx
@@ -5,15 +5,17 @@ import Image from 'next/image';
 const FirstPart = () => {
   return (
     <div className="mt-20 my-10 flex items-center">
-      <div className="flex-1">
+      <div className="flex-1 pl-2">
         <div className="text-green-950 text-[56px] font-semibold font-['Poppins'] leading-[67.20px]">
-          100% organic produce
+          100% organic <br /> produce
         </div>
-        <div>
+        <div className="w-[590px] text-stone-500 text-lg font-normal font-['Poppins'] leading-[27px] mt-4">
+          <p>
           Our commitment is to provide you with the freshest and healthiest
           fruits, vegetables, and more, cultivated without the use of synthetic
           pesticides or fertilizers. Enjoy the pure taste of nature while
           supporting a sustainable and eco-friendly approach to farming.
+          </p>
         </div>
       </div>
       <div className="w-[712px] h-[618px] ml-4">
--- green-supermarket/components/about us/secondpart.tsx
+++ green-supermarket/components/about us/secondpart.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import abtimages2 from '../../public/assets/About us images/img2.png';
-import Image from 'next/image';
+import React from "react";
+import abtimages2 from "../../public/assets/About us images/img2.png";
+import icon1 from "../../public/assets/About us images/icon1.png";
+import icon2 from "../../public/assets/About us images/icon2.png";
+import icon3 from "../../public/assets/About us images/icon3.png";
+import icon4 from "../../public/assets/About us images/icon4.png";
+
+import Image from "next/image";
 
 const SecondPart = () => {
   return (
@@ -8,16 +13,59 @@ const SecondPart = () => {
       <div className="w-[712px] h-[618px] mr-4">
         <Image src={abtimages2} alt="lady human farmer" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 pl-5">
         <div className="text-green-950 text-[56px] font-semibold font-['Poppins'] leading-[67.20px]">
-          A safe and trusted online shopping experience
+          A safe and trusted <br /> online shopping <br /> experience
         </div>
-        <div>
-          Our commitment is to provide you with the freshest and healthiest
-          fruits, vegetables, and more, cultivated without the use of synthetic
-          pesticides or fertilizers. Enjoy the pure taste of nature while
-          supporting a sustainable and eco-friendly approach to farming.
+        <div className=" mt-2">
           
+          {/* Reasons to shop */}
+          <div className="grid grid-flow-col grid-cols-2 grid-rows-3 gap-2">
+            {/* 1 Reason */}
+            <div className=" justify-start items-center gap-2 inline-flex  mt-5">
+              <Image src={icon1} alt="icon 1" />
+
+              <div className="w-56 text-zinc-900 text-lg font-semibold font-['Poppins'] leading-[27px]">
+                100% organic food
+                <p className="w-56 text-zinc-500 text-sm font-normal font-['Poppins'] leading-[21px]">
+                  100% healthy & fresh food, straight from our farmers.
+                </p>
+              </div>
+            </div>
+            {/* 2 Reason */}
+            <div className=" justify-start items-center gap-2 inline-flex  mt-5">
+              <Image src={icon2} alt="icon 2" />
+
+              <div className="w-56 text-zinc-900 text-lg font-semibold font-['Poppins'] leading-[27px]">
+              Customer Feedback
+                <p className="w-56 text-zinc-500 text-sm font-normal font-['Poppins'] leading-[21px]">
+                Our happy customer
+                </p>
+              </div>
+            </div>
+            {/* 3 Reason */}
+            <div className=" justify-start items-center gap-2 inline-flex mt-5 ">
+              <Image src={icon3} alt="icon 3" />
+
+              <div className="w-56 text-zinc-900 text-lg font-semibold font-['Poppins'] leading-[27px]">
+              Great Support 24/7
+                <p className="w-56 text-zinc-500 text-sm font-normal font-['Poppins'] leading-[21px]">
+                Instant help and support
+                </p>
+              </div>
+            </div>
+            {/* 4 Reason */}
+            <div className=" justify-start items-center gap-2 inline-flex  ">
+              <Image src={icon4} alt="icon 4" />
+
+              <div className="w-56 text-zinc-900 text-lg font-semibold font-['Poppins'] leading-[27px]">
+              100% Secure Payment
+                <p className="w-56 text-zinc-500 text-sm font-normal font-['Poppins'] leading-[21px]">
+                  Safe payment procedures
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
