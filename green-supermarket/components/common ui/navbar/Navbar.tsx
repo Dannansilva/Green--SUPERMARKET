0fa34d5d24fc3161a9e606c89e106a4b23799b79
Navishh <navishkanuwan11@gmail..com>
2023-12-13 22:58:26 +0530

    Created Navbar.jsx

--- green-supermarket/components/common ui/navbar/Navbar.tsx
+++ green-supermarket/components/common ui/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import logo from "../../../public/assets/Logo.png";
 import { CiSearch } from "react-icons/ci";
@@ -5,7 +6,25 @@ import { SlHandbag } from "react-icons/sl";
 import Image from "next/image";
 import Link from "next/link";
 import { FiPhoneCall } from "react-icons/fi";
+import { IoSearch } from "react-icons/io5";
+import { PiHandbagFill } from "react-icons/pi";
+import { FaUser } from "react-icons/fa6";
+import { RiMenu3Fill } from "react-icons/ri";
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    // console.log('buttonclick')
+  };
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
   const navItems = [
     { title: "Home", path: "/" },
     { title: "Shop", path: "/" },
@@ -16,77 +35,193 @@ const Navbar = () => {
 
   return (
     <header>
-      <div className=" flex gap-1 justify-end items-center mt-1">
-        <div className="text-stone-500  text-sm font-normal font-['Poppins'] leading-none">
-          Sign In
-        </div>
-        <div className="text-stone-500 text-sm font-normal font-['Poppins'] leading-none">
-          /
-        </div>
-        <div className="text-stone-500 text-sm font-normal font-['Poppins'] leading-none">
-          Sign Up
-        </div>
-      </div>
-      <hr className=" my-2 " />
-      <nav className=" flex justify-between mt-2">
-        {/* image */}
-        <a href="/">
-          {" "}
-          <Image src={logo} alt="logo" />
-        </a>
-        {/* search bar */}
-        <div className=" inline-flex">
-          <div className="flex items-center gap-2 border-2 p-1 rounded">
-            <form action="">
-              <div className=" flex items-center gap-3 ">
-                <div className=" text-2xl">
-                  <CiSearch />
-                </div>
-                <input
-                  type="text"
-                  name="search"
-                  id="srch"
-                  placeholder="Search"
+      <>
+        <nav>
+          <div className=" mx-2 sm:mx-2 md:mx-16 lg:mx-24 xl:mx-36 justify-between items-center">
+            {/* navbar first row */}
+            <div className="h-[42px]  hidden justify-between text-Gray text-xs items-center  sm:hidden md:flex lg:flex xl:flex md:flex-row lg:flex-row xl:flex-row">
+              {/* location */}
+              <div className="hidden sm:hidden items-center md:flex lg:flex xl:flex md:flex-row lg:flex-row xl:flex-row">
+                <Image
+                  src="/assets/mappin.png"
+                  width={15}
+                  height={18}
+                  alt="Picture of the author"
+                  className=" mr-1"
                 />
+                <p className=" sm:text-xs">
+                  Store Location: Colombo 05, Sri Lanka
+                </p>
               </div>
-            </form>
-          </div>
-          {/*  search button */}
-          <button
-            type="submit"
-            className=" bg-green-500 text-white px-4 py-1 rounded"
-          >
-            Search
-          </button>
-        </div>
+              {/* sign in/sign up */}
+              <div className="hidden md:flex lg:flex xl:flex md:flex-row lg:flex-row xl:flex-row">
+                <p className=" pr-1">Sign In</p>
+                <p className=" pr-1">/</p>
+                <p className=" pr-1">Sign Up</p>
+              </div>
+            </div>
 
-        {/* Shoping cart */}
-        <div className=" mt-2">
-          <a href="/" className=" flex items-center gap-2">
-            <SlHandbag /> LKR 570.00
-          </a>
-        </div>
-      </nav>
-      <hr className=" my-2 " />
-      {/* category */}
-      <div className=" py-4 bg-green-900 justify-between items-center inline-flex w-full lg:flex">
-        <div className="justify-start items-center gap-8 flex">
-          <ul className=" justify-start items-center gap-5 flex  text-neutral-400">
-            {navItems.map(({ title, path }) => (
-              <li key={title} className=" hover:text-white">
-                <Link href="/">{title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        {/* phone icon and number */}
-        <div className=" justify-between items-center gap-2 inline-flex">
-          <div className=" relative text-white justify-between">
-            <FiPhoneCall />
+            <hr className=" hidden md:block lg:block xl:block" />
+
+            {/* navbar second row */}
+            <div className="h-[93px] flex flex-row items-center w-full justify-between md:justify-between lg:justify-between xl:justify-between sm:-mt-6 sm:ml-2">
+              {/* logo */}
+
+              <div className="flex items-center md:h-6">
+                <Link href="/">
+                  <Image
+                    src={logo}
+                    alt="Logo"
+                    height={38}
+                    width={137}
+                    className="img-fluid md:h-full sm:w-24"
+                  />
+                </Link>
+              </div>
+
+              {/* search bar for md, lg,xl*/}
+              {/* <form className="relative justify-between w-[498px] hidden md:hidden lg:flex xl:flex md:px-4 lg:px-0 xl:px-0">
+                  <div className="w-full relative flex items-center border rounded-xl">
+                    <div className="absolute left-4">
+                      <IoSearch size={24} className="text-gray-500 text-xl" />
+                    </div>
+                    <input
+                      type="search"
+                      placeholder="Search"
+                      className="w-full pl-12 pr-4 py-1 rounded-xl focus:outline-none focus:border-transparent"
+                    />
+                    <button className="py-3 px-10 bg-green rounded-tr-xl rounded-br-xl ml-2 flex items-center justify-center bg-Green text-white w-[98px]">
+                      Search
+                    </button>
+                  </div>
+                </form> */}
+
+              {/* shopping cart */}
+
+              <div className="flex flex-row items-center focus:mr-10 justify-center mr-4">
+                {/* md search bar */}
+                <div className="  flex-row hidden sm:hidden lg:flex xl:flex md:flex ">
+                  <form
+                    action="submit"
+                    className={`  first-letter:relative mx-auto w-max items-center  ${
+                      isClicked ? "centered" : ""
+                    }`}
+                  >
+                    <div className="mr-2 relative items-center 0">
+                      <input
+                        type="search"
+                        className="peer cursor-pointer relative z-10 sm:z-10 md:z-20 lg:z-20 xl:z-30 h-12 w-12 rounded-full bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:pl-14 focus:pr-4 hover:shadow-md "
+                        onClick={handleClick}
+                      />
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+                        <IoSearch size={20} className="w-full ml-1" />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <div>
+                  <SlHandbag
+                    size={35}
+                    className="pr-3 text-black  hidden sm:hidden md:block lg:block xl:block "
+                  />
+                  <div className="flex flex-row items-center">
+                    <div className="flex flex-row justify-between justify items-center">
+                      <SlHandbag
+                        size={24}
+                        className=" pr-2 text-black sm:block md:hidden lg:hidden xl:hidden"
+                      />
+                      <p className="pr-2 text-black text-[11px] sm:block md:hidden lg:hidden xl:hidden font-semibold">
+                        Rs.0.00
+                      </p>
+                      <div className="sm:block md:hidden lg:hidden xl:hidden pr-2">
+                        <p className="text-black font-light ">|</p>
+                      </div>
+                      <FaUser
+                        size={14}
+                        className=" text-black  sm:block md:hidden lg:hidden xl:hidden"
+                      />
+                      <div className="sm:flex sm:flex-row md:hidden lg:hidden xl:hidden justify-center items-center pl-2">
+                        <button onClick={toggleMenu}>
+                          {isMenuOpen ? (
+                            <RiMenu3Fill
+                              size={18}
+                              className=" font-bold text-black sm:block md:hidden lg:hidden xl:hidden w-auto"
+                            />
+                          ) : (
+                            <RiMenu3Fill
+                              size={18}
+                              className=" font-bold text-black sm:block md:hidden lg:hidden xl:hidden"
+                            />
+                          )}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" justify-between">
+                  <div className=" items-end justify-between ">
+                    <p className="pr-1 text-Gray2 text-[11px] hidden md:hidden lg:hidden xl:hidden font-medium text-sm">
+                      Shopping cart:
+                    </p>
+                    <p className="pr-1 text-Gray text-[11px] hidden md:block lg:block xl:block font-semibold">
+                      Rs.0.00
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* md, lg ,xl menu */}
+            <div className="bg-DarkGreen h-[60px] hidden  md:flex lg:flex xl:flex lg:flex-row xl:flex-row justify-between items-center md:h-12 lg:h-[60px] xl:h-[60px] md:-mt-2 ">
+              <div className="h-full flex flex-row  px-5 md:text-sm lg:text-sm xl:text-md">
+                <ul className="flex items-center md:gap-[40px] lg:gap-[60px] xl:gap-[60px] text-Lightgray2 ">
+                  {navItems.map(({ title, path }) => (
+                    <li
+                      key={title}
+                      className="hover:text-white duration-500 ease-in-out transform cursor-pointer hover:scale-105 hover:font-semibold hover:text-md hover:flex-grow-0 "
+                    >
+                      <Link className="" href={path}>
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* sm screen menu */}
+              <div className="sm:hidden md:hidden lg:hidden xl:hidden">
+                <button onClick={toggleMenu}>
+                  <RiMenu3Fill size={18} className="font-bold text-black" />
+                </button>
+                <ul
+                  className={`bg-white text-Brown px-4 py-2 ${
+                    isMenuOpen ? "" : "hidden"
+                  }`}
+                >
+                  {navItems.map(({ title, path }) => (
+                    <li
+                      key={title}
+                      className="hover:text-white duration-500 ease-in-out transform cursor-pointer hover:scale-105 hover:font-semibold hover:text-lg"
+                    >
+                      <Link href={path}>{title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="">
+                <div className=" flex felx-row justify-between items-center px-2 text-white font-light md:text-xs">
+                  <FiPhoneCall size={14} className="text-white mr-2" />
+                  <p>+94 762630010</p>
+                </div>
+              </div>
+            </div>
+
+            {/* search bar for sm */}
           </div>
-          <div className=" text-white justify-start"> (94) 76 889 654</div>
-        </div>
-      </div>
+        </nav>
+      </>
     </header>
   );
 };
--- green-supermarket/components/home/Banner.tsx
+++ green-supermarket/components/home/Banner.tsx
@@ -4,43 +4,9 @@ import { FaArrowRight } from "react-icons/fa6";
 import Image from "next/image";
 const Banner = () => {
   return (
-    <div className="  w-full  py-[155px] bg-black bg-opacity-20 rounded-[10px] justify-start items-center inline-flex">
-      <div className="flex-col justify-start items-start gap-7 inline-flex pl-[60px] ">
-        <div className="w-[596px] text-white text-5xl font-semibold font-['Poppins'] leading-[57.60px] ">
-          Fresh & Healthy
-          <br />
-          Organic Food
-        </div>
-        <div className="justify-start items-start gap-3 inline-flex">
-          <div className="w-0.5 h-[65px] bg-green-300" />
-          <div className="flex-col justify-start items-start gap-2 inline-flex">
-            <div className="justify-start items-center gap-2 inline-flex">
-              <div className="text-white text-xl font-medium font-['Poppins'] leading-[30px]">
-                Sale up to
-              </div>
-              <div className="px-3 py-1 bg-amber-500 rounded-[5px] justify-start items-start gap-2.5 flex">
-                <div className="text-white text-xl font-semibold font-['Poppins'] leading-[30px]">
-                  30% OFF
-                </div>
-              </div>
-            </div>
-            <div className="opacity-80 text-white text-sm font-normal font-['Poppins'] leading-[21px]">
-              Free shipping on all your order.
-            </div>
-          </div>
-        </div>
-        {/* button */}
-        <div
-          className="px-10 py-4 bg-white rounded-[53px] justify-center items-center gap-4 inline-flex hover:bg-green-600 text-green-600 hover:text-white text-base
-font-semibold
-font-['Poppins']
-leading-tight"
-        >
-          <button>Shop now</button>
-          <FaArrowRight />
-        </div>
-      </div>
-    </div>
+    <>
+    
+    </>
   );
 };
 
