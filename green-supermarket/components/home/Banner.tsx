de1be2dfa6174338ebdb86f7fefcbadf7ab01fb2
Navishh <navishkanuwan11@gmail..com>
2023-12-14 12:53:18 +0530

    update Navbar.jsx

--- green-supermarket/components/home/Banner.tsx
+++ green-supermarket/components/home/Banner.tsx
@@ -4,46 +4,9 @@ import { FaArrowRight } from "react-icons/fa6";
 import Image from "next/image";
 const Banner = () => {
   return (
-<<<<<<< HEAD
-    <div className="  w-full  py-[155px] bg-black bg-opacity-20 rounded-[10px] justify-start items-center inline-flex">
-      <div className="flex-col justify-start items-start gap-7 inline-flex pl-[60px] ">
-        <div className="w-[596px] text-white text-5xl font-semibold font-['Poppins'] leading-[57.60px] ">
-          Fresh & Healthy
-          <br />
-          Organic Food
-        </div>
-        <div className="justify-start items-start gap-3 inline-flex">
-          <div className="w-0.5 h-[65px] bg-green-300" />
-          <div className="flex-col justify-start items-start gap-2 inline-flex">
-            <div className="justify-start items-center gap-2 inline-flex">
-              <div className="text-white text-xl font-medium font-['Poppins'] leading-[30px]">
-                Sale up to
-              </div>
-              <div className="px-3 py-1 bg-amber-500 rounded-[5px] justify-start items-start gap-2.5 flex">
-                <div className="text-white text-xl font-semibold font-['Poppins'] leading-[30px]">
-                  30% OFF
-                </div>
-              </div>
-            </div>
-            <div className="opacity-80 text-white text-sm font-normal font-['Poppins'] leading-[21px]">
-              Free shipping on all your order.
-            </div>
-          </div>
-        </div>
-        {/* button */}
-        <div
-          className="px-10 py-4 bg-white rounded-[53px] justify-center items-center gap-4 inline-flex hover:bg-green-600 text-green-600 hover:text-white text-lg font-semibold font-['Poppins'] leading-tight">
-          <button>Shop now</button>
-          <FaArrowRight />
-        </div>
-      </div>
-    </div>
-=======
-    <>
-    
-    </>
->>>>>>> 90214695750fa513e4777a99845c5529d161b6ec
-  );
+    <div>banner</div>
+
+    );
 };
 
 export default Banner;
