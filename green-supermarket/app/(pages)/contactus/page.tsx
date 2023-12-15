d0ea32895ff5e7c174d1e0949de93725e8ecc4ca
Shavinka06 <91057321+Shavinka06@users.noreply.github.com>
2023-12-15 20:51:49 +0530

    Left popup section in the Contact us page

---
+++ green-supermarket/app/(pages)/contactus/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from "../../../components/container/container"
+import Leftpopup from "../../../components/contact us/leftpopup"
+
+const contactus = () => {
+  return (
+    <Container>
+      <Leftpopup /> 
+      </Container>
+  
+  ) 
+}
+
+export default contactus
