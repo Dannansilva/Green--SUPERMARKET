a1b93576b2ce102398cff13d8b8900d2cb5acde1
Navishh <navishkanuwan11@gmail..com>
2023-12-16 05:33:54 +0530

    Created PopCategories.tsx

--- green-supermarket/public/assets/products images/Categories.tsx
+++ green-supermarket/public/assets/products images/Categories.tsx
@@ -1,48 +1,7 @@
-import React from "react";
-
-const Categories = [
-    {
-        id:1,
-        image: "assets/products images/Categoryimages/image 1.png",
-        catname: "Fresh Fruit"
-    }
-]
 
 
 
 
-
-
-// <div className=" mx-2 sm:mx-2 md:mx-16 lg:mx-24 xl:mx-36 md:mt-[60px] sm:-mt-2 ">
-        
-//         {/* First row - popular categories row */}
-//         <div className="flex items-center justify-between">
-//             <div>
-//                 <h2 className=" text-black font-semibold md:text-2xl sm:text-xl">Popular Categories</h2>
-//             </div>
-//             <div>
-//                 <button className=" text-Green text-base font-medium hover:border hover:shadow-lg px-4 py-2 text-center hover:rounded-md">{`View All ->`}</button>
-//             </div>
-//         </div>
-
-//         {/* Second row  */}
-//         <div className="flex sm:flex-col">
-//             <div className="flex flex-col">
-//                 <div className="border border-Lightgray rounded">
-//                     <Image
-//                     src={""}
-//                     alt=""
-//                     width={23}
-//                     height={23}
-//                     className=""
-//                     />
-
-                    
-//                 </div>
-//                 <div></div>
-//             </div>
-//         </div>
-//       </div>
 
 
 
