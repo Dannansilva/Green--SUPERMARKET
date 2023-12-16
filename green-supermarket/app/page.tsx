c156c8d8a287bba5064033f12e122e110c378e38
Navishh <navishkanuwan11@gmail..com>
2023-12-16 20:15:09 +0530

    Topsales.tsx

--- green-supermarket/app/page.tsx
+++ green-supermarket/app/page.tsx
@@ -1,13 +1,16 @@
 import Navbar from "@/components/common ui/navbar/Navbar"
 import Banner from "@/components/home/Banner"
 import PopCategories from "@/components/home/PopCategories"
+import TopSales from "@/components/home/TopSales"
 
 export default function Home() {
   return (
    <div>
     
     <Banner/>
+    <TopSales/>
     <PopCategories/>
+    
    </div>
   )
 }
