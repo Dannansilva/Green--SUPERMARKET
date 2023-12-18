779fdf24de94698f29eee7db9e38ac83c4040479
Navishh <navishkanuwan11@gmail..com>
2023-12-18 15:56:31 +0530

    Created Bakery.tsx

---
+++ green-supermarket/app/(pages)/shop/Bakery/Bakery.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { SlHandbag } from "react-icons/sl";
+
+   
+       
+        
+        
+        const Bakery = [
+        
+            {
+                id:1,
+                name: <p className=" text-Lightgray hover:text-DarkGreen ">Bread</p>,
+                image:"/assets/ShopPage/Bakery/Homemade crunchy bread with grains.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },{
+                id:2,
+                name:<p className="  text-Lightgray hover:text-DarkGreen ">Garlic bread</p>,
+                image:"/assets/ShopPage/Bakery/Homemade crunchy bread with grains-1.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:3,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Hamburger bun</p>,
+                image:"/assets/ShopPage/Bakery/Fast food and junk food concept.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:4,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Submarine bun</p>,
+                image:"/assets/ShopPage/Bakery/Delicious sandwich.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:5,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Cinnamon roll</p>,
+                image:"/assets/ShopPage/Bakery/Cinnamon roll.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:6,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Chocolate chip muffin</p>,
+                image:"/assets/ShopPage/Bakery/Cupcake with grated chocolate.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },{
+                id:7,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Chocolate cake</p>,
+                image:"/assets/ShopPage/Bakery/Chocolate cake with chocolate sprinkles.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:8,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Fruit cake</p>,
+                image:"/assets/ShopPage/Bakery/Vanilla cake dessert with strawberry on top.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:8,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Chocolate chip Cookie</p>,
+                image:"/assets/ShopPage/Bakery/Chocolate chip cookies.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:8,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Chocolate Brownies</p>,
+                image:"/assets/ShopPage/Bakery/Brownies.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+        ] 
+         
+        
+        export default Bakery     
+
+
