0d855e2c3ffda67de24abc5e31add0f0f2801b50
Navishh <navishkanuwan11@gmail..com>
2023-12-18 21:50:25 +0530

    Created Spices.tsx

---
+++ green-supermarket/app/(pages)/shop/Spices/Spices.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { SlHandbag } from "react-icons/sl";
+
+const spices = [
+
+    
+        {
+            id:1,
+            name: <p className=" text-Lightgray hover:text-DarkGreen ">Chilli powder</p>,
+            image:"/assets/ShopPage/Spices/Chilli pepper on white surface.png",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },{
+            id:2,
+            name:<p className="  text-Lightgray hover:text-DarkGreen ">Curry powder</p>,
+            image:"/assets/ShopPage/Spices/currypowder.png",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:3,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Tumeric powder</p>,
+            image:"/assets/ShopPage/Spices/tumericpowder.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:4,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Pepper</p>,
+            image:"/assets/ShopPage/Spices/pepper.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:5,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Salt</p>,
+            image:"/assets/ShopPage/Spices/salt.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:6,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Chilli flakes</p>,
+            image:"/assets/ShopPage/Spices/chilliflakes.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },{
+            id:7,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Ginger</p>,
+            image:"/assets/ShopPage/Spices/ginger.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:8,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Tamarind</p>,
+            image:"/assets/ShopPage/Spices/Tamarind.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:9,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Cloves</p>,
+            image:"/assets/ShopPage/Spices/cloves.jpg",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        {
+            id:10,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Cinnamon</p>,
+            image:"/assets/ShopPage/Spices/Cinnamon sticks wrapped with a rustic thread.png",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+        
+]
+export default spices
