1576ef2cf89577471d310de252d81d589e0ede09
Navishh <navishkanuwan11@gmail..com>
2023-12-18 16:27:31 +0530

    Created Meat.tsx

---
+++ green-supermarket/app/(pages)/shop/Meat/Meat.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { SlHandbag } from "react-icons/sl";
+
+   
+       
+        
+        
+        const Meat = [
+        
+            {
+                id:1,
+                name: <p className=" text-Lightgray hover:text-DarkGreen ">Chicken</p>,
+                image:"/assets/ShopPage/meat/Raw chicken meat.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },{
+                id:2,
+                name:<p className="  text-Lightgray hover:text-DarkGreen ">Beef</p>,
+                image:"/assets/ShopPage/meat/Raw steak on white paper.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:3,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Mutton</p>,
+                image:"/assets/ShopPage/meat/Raw steak on white paper-1.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:4,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Pork</p>,
+                image:"/assets/ShopPage/meat/Raw meat.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+    
+        {
+            id:5,
+            name:<p className=" text-Lightgray hover:text-DarkGreen ">Turkey</p>,
+            image:"/assets/ShopPage/meat/turkey.png",
+            price:"LKR 100.00",
+            image2: <SlHandbag/>
+        },
+            {
+                id:6,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Sausage</p>,
+                image:"/assets/ShopPage/meat/Grilled sausage.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:7,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Prawns</p>,
+                image:"/assets/ShopPage/meat/Delicious shrimps.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },{
+                id:8,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Crabs</p>,
+                image:"/assets/ShopPage/meat/Steamed crabs on white background ..png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:9,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Fish</p>,
+                image:"/assets/ShopPage/meat/Raw salmon.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+            {
+                id:10,
+                name:<p className=" text-Lightgray hover:text-DarkGreen ">Eggs</p>,
+                image:"/assets/ShopPage/meat/Brown eggs.png",
+                price:"LKR 100.00",
+                image2: <SlHandbag/>
+            },
+        ] 
+         
+        
+        export default Meat    
+
+
